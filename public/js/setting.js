d50fbb09e12f543022d8732a1916b58a0dcfb823
heejuShin <21800412@handong.edu>
2020-07-30 02:15:49 +0900

    setting 수정

---
+++ public/js/setting.js
@@ -0,0 +1,6 @@
+document.querySelector('#confirm1').addEventListener('click', function(){
+  var s1 = document.getElementById("setting1");
+  s1.style.display = "none";
+  var s2 = document.getElementById("setting2");
+  s2.style.display = "inline-block";          
+});
