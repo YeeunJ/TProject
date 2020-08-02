b66da70aaf564d0b4486c66766d8565729263b73
heejuShin <21800412@handong.edu>
2020-08-03 02:20:39 +0900

    setting view add

--- public/js/setting.js
+++ public/js/setting.js
@@ -1,6 +1,14 @@
+/*
 document.querySelector('#confirm1').addEventListener('click', function(){
   var s1 = document.getElementById("setting1");
   s1.style.display = "none";
-  var s2 = document.getElementById("setting2");
-  s2.style.display = "inline-block";          
+  var s2 = document.getElementById("setting3");
+  s2.style.display = "inline-block";
+});*/
+
+document.querySelector('#edit_camera').addEventListener('click', function(){
+  var view = document.getElementById("setting3");
+  view.style.display = "none";
+  var edit = document.getElementById("setting2");
+  edit.style.display = "inline-block";
 });
