1affdbfae09d84954290e68056d42625c749d199
YeeunJ <21800669@handong.edu>
2020-08-03 02:42:44 +0900

    url update

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json({limit:5000000}));
 app.use(bodyParser.urlencoded({limit:5000000, extended:true, parameterLimit: 5000000}));
 // Routes
-app.use('/', require('./routes/admin'));
-app.use('/basic', require('./routes/basic'));
+app.use('/setting', require('./routes/admin'));
+app.use('/', require('./routes/basic'));
 app.use('/api', require('./routes/api'));
 
 // Port setting
