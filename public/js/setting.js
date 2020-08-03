52359453260f786bee0ff05ae3ac757f9acb64aa
heejuShin <21800412@handong.edu>
2020-08-03 12:47:30 +0900

    setting 하나로 합침

--- public/js/setting.js
+++ public/js/setting.js
@@ -7,15 +7,37 @@ document.querySelector('#confirm1').addEventListener('click', function(){
 });*/
 
 document.querySelector('#edit_camera').addEventListener('click', function(){
-  var view = document.getElementById("setting_view");
-  view.style.display = "none";
-  var edit = document.getElementById("setting_camera");
-  edit.style.display = "inline-block";
+  var btn = document.getElementById("confirm_camera");
+  btn.style.display = "inline-block";
+  var edit_camera = document.getElementById("edit_camera");
+  edit_camera.style.display = "none";
+  var edit_image = document.getElementById("edit_image");
+  edit_image.style.display = "none";
+  var f=document.getElementById("form_setting");
+  f.sizeW.readOnly = false;
+  f.sizeH.readOnly = false;
+  f.resizeW.readOnly = false;
+  f.resizeH.readOnly = false;
+  f.camNum.readOnly = false;
+  f.sizeW.style.color= "white";
+  f.sizeH.style.color= "white";
+  f.resizeW.style.color= "white";
+  f.resizeH.style.color= "white";
+  f.camNum.style.color= "white";
 });
 
 document.querySelector('#edit_image').addEventListener('click', function(){
-  var view = document.getElementById("setting_view");
-  view.style.display = "none";
-  var edit = document.getElementById("setting_image");
-  edit.style.display = "inline-block";
+  var btn = document.getElementById("confirm_image");
+  btn.style.display = "inline-block";
+  var edit_camera = document.getElementById("edit_camera");
+  edit_camera.style.display = "none";
+  var edit_image = document.getElementById("edit_image");
+  edit_image.style.display = "none";
+  var f=document.getElementById("form_setting");
+  f.saveNum.readOnly = false;
+  f.savePeriod.readOnly = false;
+  f.saveInterval.readOnly = false;
+  f.saveNum.style.color= "white";
+  f.savePeriod.style.color= "white";
+  f.saveInterval.style.color= "white";
 });
